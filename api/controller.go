002f94f1fbd5a9924c2dbd3af00cd79e63c4dc1b
XuXiaoRui <1531793838@qq.com>
2026-03-12 23:54:33 +0800

    新增统一JSON响应封装函数JSONBack

---
+++ api/controller.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JsonBack(c *gin.Context, message string, ret int, data interface{}) {
+	if ret == 0 {
+		if data != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    200,
+				"message": message,
+				"data":    data,
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    200,
+				"message": message,
+			})
+		}
+	} else if ret == -2 {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    400,
+			"message": message,
+		})
+	} else if ret == -1 {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": message,
+		})
+	}
+}
