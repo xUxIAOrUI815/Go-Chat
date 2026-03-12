e836bc99f302a147fa1fb34619a1a81ce08d9169
XuXiaoRui <1531793838@qq.com>
2026-03-12 20:01:56 +0800

    fix:修改函数名

--- pkg/util/random/random_int.go
+++ pkg/util/random/random_int.go
@@ -11,6 +11,6 @@ func GetRandomInt(len int) int { // 生成指定长度的随机整数
 	return rand.Intn(9*int(math.Pow(10, float64(len-1)))) + int(math.Pow(10, float64(len-1)))
 }
 
-func GetNowAndRandomString(len int) string { // 生成日期+指定的随机数组合字符串
+func GetNowAndLenRandomString(len int) string { // 生成日期+指定的随机数组合字符串
 	return time.Now().Format("20060102") + strconv.Itoa(GetRandomInt(len))
 }
