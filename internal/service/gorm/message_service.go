24c03df23e88432abbfbeb1da26e508609c958b5
XuXiaoRui <1531793838@qq.com>
2026-03-13 21:48:06 +0800

    feat(service):实现消息服务的核心业务功能

---
+++ internal/service/gorm/message_service.go
@@ -0,0 +1,191 @@
+package gorm
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"go-chat/internal/config"
+	"go-chat/internal/dao"
+	"go-chat/internal/dto/respond"
+	"go-chat/internal/model"
+	myredis "go-chat/internal/service/redis"
+	"go-chat/pkg/constants"
+	"go-chat/pkg/zlog"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+type messageService struct {
+}
+
+var MessageService = new(messageService)
+
+// GetMessageList 获取聊天记录
+func (m *messageService) GetMessageList(userOneId, userTwoId string) (string, []respond.GetMessageListRespond, int) {
+	rspString, err := myredis.GetKeyNilIsErr("message_list_" + userOneId + "_" + userTwoId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) { // 如果缓存没查到
+			zlog.Info(err.Error())
+			zlog.Info(fmt.Sprintf("%s %s", userOneId, userTwoId))
+			var messageList []model.Message
+			if res := dao.GormDB.Where("(send_id = ? AND receive_id = ?) OR (send_id = ? AND receive_id = ?)", userOneId, userTwoId, userTwoId, userOneId).Find(&messageList); res.Error != nil {
+				zlog.Error(res.Error.Error())
+				return constants.SYSTEM_ERROR, nil, -1
+			}
+			var rspList []respond.GetMessageListRespond
+			for _, message := range messageList {
+				rspList = append(rspList, respond.GetMessageListRespond{
+					SendId:     message.SendId,
+					SendName:   message.SendName,
+					SendAvatar: message.SendAvatar,
+					ReceiveId:  message.ReceiveId,
+					Content:    message.Content,
+					Url:        message.Url,
+					Type:       message.Type,
+					FileType:   message.FileType,
+					FileName:   message.FileName,
+					FileSize:   message.FileSize,
+					CreatedAt:  message.CreatedAt.Format("2006-01-02 15:04:05"),
+				})
+			}
+			// TODO: 聊天记录预热逻辑 暂时不启用
+			//rspString, err := json.Marshal(rspList)
+			//if err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			//if err := myredis.SetKeyEx("message_list_"+userOneId+"_"+userTwoId, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			return "获取聊天记录成功", rspList, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+	// 缓存命中 直接反序列化之后返回
+	var rsp []respond.GetMessageListRespond
+	if err := json.Unmarshal([]byte(rspString), &rsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "获取聊天记录成功", rsp, 0
+}
+
+// GetGroupMessageList 获取群聊聊天记录
+func (m *messageService) GetGroupMessageList(groupId string) (string, []respond.GetGroupMessageListRespond, int) {
+	rspString, err := myredis.GetKeyNilIsErr("group_message_list_" + groupId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) { // 如果在redis中没有找到
+			var messageList []model.Message
+			if res := dao.GormDB.Where("receive_id = ?", groupId).Order("created_at ASC").Find(&messageList); res.Error != nil {
+				zlog.Error(res.Error.Error())
+				return constants.SYSTEM_ERROR, nil, -1
+			}
+			var rspList []respond.GetGroupMessageListRespond
+			for _, message := range messageList {
+				rsp := respond.GetGroupMessageListRespond{
+					SendId:     message.SendId,
+					SendName:   message.SendName,
+					SendAvatar: message.SendAvatar,
+					ReceiveId:  message.ReceiveId,
+					Content:    message.Content,
+					Url:        message.Url,
+					Type:       message.Type,
+					FileType:   message.FileType,
+					FileName:   message.FileName,
+					FileSize:   message.FileSize,
+					CreatedAt:  message.CreatedAt.Format("2006-01-02 15:04:05"),
+				}
+				rspList = append(rspList, rsp)
+			}
+
+			//rspString, err := json.Marshal(rspList)
+			//if err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			//if err := myredis.SetKeyEx("group_messagelist_"+groupId, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			return "获取聊天记录成功", rspList, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+	var rsp []respond.GetGroupMessageListRespond
+	if err := json.Unmarshal([]byte(rspString), &rsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "获取聊天记录成功", rsp, 0
+}
+
+// UploadAvatar 上传头像
+func (m *messageService) UploadAvatar(c *gin.Context) (string, int) {
+	if err := c.Request.ParseMultipartForm(constants.FILE_MAX_SIZE); err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	mForm := c.Request.MultipartForm
+	for key, _ := range mForm.File {
+		file, fileHeader, err := c.Request.FormFile(key)
+		if err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		defer file.Close()
+		zlog.Info(fmt.Sprintf("文件名：%s，文件大小：%d", fileHeader.Filename, fileHeader.Size))
+		// 原来Filename应该是213451545.xxx，将Filename修改为avatar_ownerId.xxx
+		ext := filepath.Ext(fileHeader.Filename)
+		zlog.Info(ext)
+		localFileName := config.GetConfig().StaticAvatarPath + "/" + fileHeader.Filename
+		out, err := os.Create(localFileName)
+		if err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		defer out.Close()
+		if _, err := io.Copy(out, file); err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		zlog.Info("完成文件上传")
+	}
+	return "上传成功", 0
+}
+
+// UploadFile 上传文件
+func (m *messageService) UploadFile(c *gin.Context) (string, int) {
+	if err := c.Request.ParseMultipartForm(constants.FILE_MAX_SIZE); err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	mForm := c.Request.MultipartForm
+	for key, _ := range mForm.File {
+		file, fileHeader, err := c.Request.FormFile(key)
+		if err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		defer file.Close()
+		zlog.Info(fmt.Sprintf("文件名：%s，文件大小：%d", fileHeader.Filename, fileHeader.Size))
+		// 原来Filename应该是213451545.xxx，将Filename修改为avatar_ownerId.xxx
+		ext := filepath.Ext(fileHeader.Filename)
+		zlog.Info(ext)
+		localFileName := config.GetConfig().StaticFilePath + "/" + fileHeader.Filename
+		out, err := os.Create(localFileName)
+		if err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		defer out.Close()
+		if _, err := io.Copy(out, file); err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1
+		}
+		zlog.Info("完成文件上传")
+	}
+	return "上传成功", 0
+}
